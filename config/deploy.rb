dc2b3769738b5ecc34a1e12cb76fdc9dd7673bc9
ellenlee <leechinghui.tw@gmail.com>
2016-10-06 23:04:42 +0800

    home deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:ellenlee/outfit.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/outfit'
+set :deploy_to, '/home/deploy/outfit'
 
 # Default value for :scm is :git
 # set :scm, :git
