306677de813246be13fb961affede85f92be121d
ellenlee <leechinghui.tw@gmail.com>
2016-10-23 02:08:39 +0800

    routes update

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem "rails-i18n"
 
+gem 'rollbar'
+gem 'airbrake', '~> 5.6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,23 @@
 class PhotosController < ApplicationController
-	before_action :set_asker
+	# before_action :set_asker
 
 	def new
-		@photo = @asker.photos.new
+		@photo = Photo.new
 	end
 
 	def create
+		set_asker
 		@photo = @asker.photos.new(photo_params)
 		if @photo.save
 			redirect_to asker_photo_path(@asker, @photo)
+		else
+			redirect_to new_asker_photo_path(@asker), alert: "有點問題⋯⋯只能上傳圖片哦！"
 		end
 	end
 
 	def show
-		@photo = Photo.find(params[:id])
+		set_asker
+		@photo = @asker.photos.find(params[:id])
 		@request = @photo.requests.build
 		@options = Option.all
 	end
